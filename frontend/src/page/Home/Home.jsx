e7e1c225b09fd74c1d12d363179f3232323305f7
isidoraadj <isidoradjkg@gmail.com>
2026-02-02 19:25:01 +0100

    Feature sekcija

--- frontend/src/page/Home/Home.jsx
+++ frontend/src/page/Home/Home.jsx
@@ -1,11 +1,41 @@
 import React from 'react';
 import Slider from '../../components/Slider/Slider';
+import { Col, Container, Row } from 'react-bootstrap';
+import { featureData } from '../../utils/data';
  
 const Home = () => {
   return (
-<>
-  <Slider />
-</>
+    <>
+      <Slider />
+      <section className="py-5">
+        <Container>
+          <Row>
+          {
+            featureData.map((val, index)=>{
+              return (
+                <Col md={6} lg={3} sm={6} key={index} >
+                  <div className="d-flex align-items-start">
+                    <div className="mb-3 ms-3">
+                      <img 
+                        src={val.image}
+                        className="img-fluid"
+                        alt={val.title}
+                      />
+                      </div>
+                      <div className="text">
+                        <h3 className="sub-heading mb-1"> {val.title}</h3>
+                        <p className="body-text"> {val.text}</p>
+                      </div>
+                  </div>
+                </Col>
+              )
+            })
+          }
+          </Row>
+        </Container>
+      </section>
+  
+    </>
   )
 }
  
